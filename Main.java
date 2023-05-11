ddd76288d611cb5f85bfe01ea36ec81f286f4308
mallozzi <2130128@upv.edu.mx>
2023-05-11 12:04:02 -0600

    correcto

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main {
 
     public static void main(String[] args) {
+        // hacer string un int
+        int a = Integer.parseInt(args [0]); //_int variable primitiva/  Integer clase con atributos y metodos
+        int b = Integer.parseInt(args [1]); // metodo string a integer
 
+        //
+        SumaDosNumeros obj = new SumaDosNumeros(); // instacia de la clase x -> "SumaDosNumeros"     / Separa el espacio de memoria
+        System.out.println(obj.suma(a, b));
+        
     }
 }
